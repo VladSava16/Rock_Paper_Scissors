dc6f04d7a26b337ea91ed895cbf494c8d69d8243
Vlad Sava <savavlad82@gmail.com>
2022-03-20 12:49:27 +0200

    Add computerSelection and game rules function
    Added an event listener of every button and added a function for
    computerSelection.
    Also added 2 variables for the playerScore and computerScore.

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ function computerPlay(){
         return "paper";
     return "scissors";
 }
+
 function play(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase() == "rock"){
+    if(playerSelection == "rock"){
         if(computerSelection == "rock")
             return -1;
         else if(computerSelection == "paper")
             return 0;
         return 1;
     }
-    else if(playerSelection.toLowerCase() == "paper"){
+    else if(playerSelection== "paper"){
         if(computerSelection == "rock")
             return 1;
         else if(computerSelection == "paper")
             return -1;
         return 0;
     }
-    else if(playerSelection.toLowerCase() == "scissors"){
+    else if(playerSelection == "scissors"){
         if(computerSelection == "rock")
             return 0;
         else if(computerSelection == "paper")
@@ -30,27 +31,12 @@ function play(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    for(let i = 1; i <= 5; i++){
-        let playerSelection = prompt("Enter your weapon");
-        let computerSelection = computerPlay();
 
-        let result = play(playerSelection, computerSelection);
-        if(result == 1){
-            console.log("You win.");
-            playerScore++;
-        }
-        else if(result == 0){
-            console.log("You lose.");
-            computerScore++;
-        }
-        else
-            console.log("It's a tie.");
-    }
-    console.log(`The score is ${playerScore} to ${computerScore}.`);
-}
+const btns = document.querySelectorAll('button');
+
+btns.forEach(choice => {
+    choice.addEventListener('click', game(e))
+});
 
 let playerScore = 0;
 let computerScore = 0;
-
-game();
